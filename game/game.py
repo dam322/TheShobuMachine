52de4b857ac8cfdd5344a579741927dc2ea1be99
Ian2012 <crisgarta8@gmail.com>
2021-03-02 18:02:32 -0500

    Sistema de turno modificado. Movimiento de las fichas permitido. Optimización del código.

--- game/game.py
+++ game/game.py
@@ -6,15 +6,22 @@ from models.board import Board
 def main():
     pygame.init()
     pygame.display.set_caption("The Shobu Machine")
+    # Tamaño de las piezas
     piece_size = 64
+    # Espacio entre los trableros
     space_between_boards = 40
+    # Distancia entre el borde la pantalla y los tableros
     padding = 30
+    # Distancia para centrar las piezas y que no ocupen el espacio del tablero
     temp_to_center = 20
 
+    # Calcular el tamaño de la pantalla
     screen = pygame.display.set_mode((8 * piece_size + space_between_boards + 2 * padding,
                                       8 * piece_size + space_between_boards + 2 * padding))
+    # Reloj auxiliar
     clock = pygame.time.Clock()
 
+    # Iniciar los 4 tableros
     board1 = Board(padding,
                    padding,
                    piece_size, temp_to_center)
@@ -30,67 +37,101 @@ def main():
 
     boards = [board1, board2, board3, board4]
     running = True
-    turn = True
 
+    # Turno del jugador blano
+    turn = True
+    pieces_to_highligth = []
+    # Bucle del juego
     while running:
         # Capturar eventos
         for event in pygame.event.get():
-
+            # Terminar el proceso cuando se cierre la ventana
             if event.type == pygame.QUIT:
                 running = False
-
+            # Evento del ratón
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:  # Click derecho
+                # Click izquierdo
+                if event.button == 1:
+                    # Capturar la posición del mouse, crear un rectangulo y verificar en cual tablero hizo click
                     mx, my = pygame.mouse.get_pos()
                     mouse_rect = pygame.Rect(mx, my, 1, 1)
                     board = mouse_collide(mouse_rect, boards)
-                    if board is not None:
-                        player = 1 if turn else 2
 
-                        x, y = get_first_click_mouse(mouse_rect, board, player)
+                    if board is None:
+                        continue
 
-                        if x == -1:
-                            break
-                        print("fuera ", x, y)
-                        clicked = False
-                        while not clicked:
-                            event = pygame.event.wait()
-                            if event.type == pygame.MOUSEBUTTONDOWN:
-                                mx, my = pygame.mouse.get_pos()
-                                mouse_rect = pygame.Rect(mx, my, 1, 1)
+                    player = 1 if turn else 2
+                    piece_to_move = get_piece_coordinates_clicked(mouse_rect, board, player)
+                    # Si clickeó en una ficha que no sea del jugador actual no debe hacer nada
+                    if piece_to_move is None:
+                        break
+                    coordinates = []
 
-                                x_1, y_1 = get_move_mouse(mouse_rect, board)
-                                if x_1 == -2:
-                                    continue
+                    for line in board.map:
+                        for piece in line:
+                            if (piece.x, piece.y) in piece_to_move.valid_moves \
+                                    and (piece.value != player):
+                                pieces_to_highligth.append(piece)
+                                coordinates.append((piece.x, piece.y))
 
-                                print("dentro ", x_1, y_1)
-                                if x_1 == x and y_1 == y:
-                                    print("son las mismas")
-                                else:
-                                    print("no son las mismas")
-                                    clicked = True
-                                    turn = not turn
+                    draw_highlights(screen, pieces_to_highligth)
+                    pygame.display.update()
+                    clicked = False
+                    while not clicked:
+                        # Esperar al siguiente click para hacer el movimiento
+                        event = pygame.event.wait()
+                        # Si el evento no es un click entonces debe esperar al siguiente evento
+                        if event.type != pygame.MOUSEBUTTONDOWN:
+                            continue
+
+                        mx, my = pygame.mouse.get_pos()
+                        mouse_rect = pygame.Rect(mx, my, 1, 1)
+
+                        # Verificar que sea un movimiento válido
+                        piece_where_is_moved = get_next_piece_coordinates_clicked(mouse_rect, board)
+
+                        # Verificar que clickee en una ficha del mismo tablero
+                        if piece_where_is_moved is None:
+                            print("Click en ficha equivocada")
+                            continue
+                        # Verificar que la ficha clickeada no sea la misma que esocgió al principio
+                        if piece_to_move.get_coordinates() == piece_where_is_moved.get_coordinates():
+                            print("Es la misma ficha")
+                            continue
+
+                        # Si la posición no está en los movimientos válidos entonces no haga nada
+                        if piece_where_is_moved.get_coordinates() not in coordinates:
+                            print("Coordenada incorrecta: ", piece_where_is_moved.get_coordinates(), coordinates)
+                            continue
+
+                        piece_to_move.move(piece_where_is_moved)
+                        clicked = True
+                        turn = not turn
+                        pieces_to_highligth = []
         update(boards)
-        draw(screen, boards, piece_size, turn)
+        draw(screen, boards, piece_size, turn, pieces_to_highligth)
         clock.tick(60)
 
 
-def get_first_click_mouse(mouse_rect, board, player):
+# Obtiene la coordenada del primer movimiento
+def get_piece_coordinates_clicked(mouse_rect, board, player):
     for y in range(4):
         for x in range(4):
             if mouse_rect.colliderect(board.map[y][x].rect) and board.map[y][x].value == player:
-                return x, y
-    return -1, -1
+                return board.map[y][x]
+    return None
 
 
-def get_move_mouse(mouse_rect, board):
+# Obtiene la coordenada del movimiento hacia donde quiere moverse
+def get_next_piece_coordinates_clicked(mouse_rect, board):
     for y in range(4):
         for x in range(4):
             if mouse_rect.colliderect(board.map[y][x].rect):
-                return x, y
-    return -2, -2
+                return board.map[y][x]
+    return None
 
 
+# Obtiene el tablero donde dió click el jugador
 def mouse_collide(mouse_rect, boards):
     for x in boards:
         if mouse_rect.colliderect(x):
@@ -98,7 +139,8 @@ def mouse_collide(mouse_rect, boards):
     return None
 
 
-def draw(screen, boards, piece_size, turn):
+# Dibujar todos los elementos del mapa
+def draw(screen, boards, piece_size, turn, coordinates):
     # Fondo
     # screen.fill((38, 163, 144))
     if turn:
@@ -109,12 +151,22 @@ def draw(screen, boards, piece_size, turn):
     for board in boards:
         draw_board(screen, piece_size, board)
 
+    draw_highlights(screen, coordinates)
 
+
+# Dibujar el tablero y sus elementos
 def draw_board(screen, piece_size, board):
     temp_line = 4
     board.draw(screen, piece_size, temp_line)
 
 
+# TODO Recfatorizar este comentario: Dibujar en gris las fichas que cumplan
+def draw_highlights(screen, coordinates):
+    for piece in coordinates:
+        piece.draw_highlight(screen)
+
+
+# Actualiza el tablero y sus elementos
 def update(boards):
     for board in boards:
         board.update()
--- models/piece.py
+++ models/piece.py
@@ -8,6 +8,25 @@ class Piece:
                                 init_y + y * piece_size + temp_to_center / 2,
                                 piece_size - temp_to_center,
                                 piece_size - temp_to_center)
+        self.x = x
+        self.y = y
+        self.valid_moves = [(x + 1, y),  # Derecha
+                            (x - 1, y),  # Izquierda
+                            (x, y + 1),  # Superior
+                            (x, y - 1),  # Inferior
+                            (x + 1, y + 1),  # Inferior derecha
+                            (x - 1, y - 1),  # Superior izquierda
+                            (x + 1, y - 1),  # Inferior izquierda
+                            (x - 1, y + 1),  # Superior derecha
+                            (x + 2, y),  # Derecha 2
+                            (x - 2, y),  # Izquierda 2
+                            (x, y + 2),  # Superior 2
+                            (x, y - 2),  # Inferior 2
+                            (x + 2, y + 2),  # Inferior derecha 2
+                            (x - 2, y - 2),  # Superior izquierda 2
+                            (x + 2, y - 2),  # Inferior izquierda 2
+                            (x - 2, y + 2),  # Superior derecha 2
+                            ]
 
     def draw(self, screen):
         if self.value == 1:
@@ -15,5 +34,22 @@ class Piece:
         elif self.value == 2:
             pygame.draw.rect(screen, (0, 0, 0), self.rect)
 
+    def draw_highlight(self, screen):
+        # if self.value == 1:
+        pygame.draw.rect(screen, (155, 155, 155), self.rect)
+        # elif self.value == 2:
+        # pygame.draw.rect(screen, (225, 225, 255), self.rect)
+
     def update(self):
         pass
+
+    def is_valid_moves(self, x, y):
+        return (x, y) in self.valid_moves
+
+    def move(self, piece):
+        # TODO Definir la lógica del movimiento. Por ahora es sólo un intercambio que no sirve de mucho
+        self.value, piece.value = piece.value, self.value
+        # self.valid_moves, piece.valid_moves = piece.valid_moves, self.valid_moves
+
+    def get_coordinates(self):
+        return self.x, self.y
